4756fdbf32924044dacb70b9d743cd5ece71efbd
Ken Moore <moorekou@gmail.com>
2016-08-03 16:16:20 -0400

    Clean up some help text on the installer.

--- src-qt5/pc-installgui/helpText.h
+++ src-qt5/pc-installgui/helpText.h
@@ -4,10 +4,10 @@
 #include <QObject>
 
 #define HELPTEXT0 QObject::tr("This screen allows you to select the language you wish to perform the install with.")
-#define HELPTEXT1 QObject::tr("This screen allows you to select the type of system you wish to install. Clicking customize will allow you to change the selected packages for your system.")
+#define HELPTEXT1 QObject::tr("This screen allows you to select the type of system you wish to install.")
 #define HELPTEXT2 QObject::tr("This screen will display the currently selected disk options. Clicking customize will allow you to change the target disk, file-systems and more.")
 #define HELPTEXT3 QObject::tr("Your system is now installing! Go grab a cup of coffee and relax, the installer will notify you when it is finished.")
-#define HELPTEXT4 QObject::tr("The system is now finished installing! Click Finish to reboot, and be sure to eject any install media before booting into your new system.")
+#define HELPTEXT4 QObject::tr("The system is now finished installing! Click Finish to return to the text menu where you can reboot the system, and be sure to eject any install media before booting into your new system.")
 #define HELPTEXT5 QObject::tr("")
 #define HELPTEXT6 QObject::tr("")
 #define HELPTEXT7 QObject::tr("")
