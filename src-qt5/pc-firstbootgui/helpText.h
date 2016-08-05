a167c8e43a3f82bd30bbf868e036b6946449679f
Ken Moore <ken@pcbsd.org>
2016-08-05 14:37:29 -0400

    Fix the help text in the firstboot UI so that it references SysAdm instead of the Control Panel.

--- src-qt5/pc-firstbootgui/helpText.h
+++ src-qt5/pc-firstbootgui/helpText.h
@@ -6,7 +6,7 @@
 #define HELPTEXT0 QObject::tr("This screen allows you to select the system language you wish to use.")
 #define HELPTEXT1 QObject::tr("This screen lets you pick the default timezone for your computer.")
 #define HELPTEXT2 QObject::tr("On this screen please enter the root password you wish to use. This password is used for full system access, and should be something secure.")
-#define HELPTEXT3 QObject::tr("On this screen you will need to create a user to log into your desktop. Additional users may be created in the Control Panel.")
+#define HELPTEXT3 QObject::tr("On this screen you will need to create a user to log into your desktop. Additional users may be created in the SysAdm utility later.")
 #define HELPTEXT4 QObject::tr("This screen allows you to test your audio playback device, and set that device as the default for all audio. This may be changed on-demand through the audio mixer later as well.")
 #define HELPTEXT5 QObject::tr("On this screen you may connect to an available wifi network. If you have a hidden network, it may be added later in the Network Manager.")
 #define HELPTEXT6 QObject::tr("Optional configuration changes or services may now be enabled as needed. Simply check or uncheck any options and click next to continue.")
