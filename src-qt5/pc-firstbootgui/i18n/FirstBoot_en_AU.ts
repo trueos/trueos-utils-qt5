3eed257a8ccd1732fdff8368477c3f055ddb98ec
ZackaryWelch <welch.zackary@gmail.com>
2017-07-31 10:44:26 -0400

    Added Australian English translation files

---
+++ src-qt5/pc-firstbootgui/i18n/FirstBoot_en_AU.ts
@@ -0,0 +1,384 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Installer</name>
+    <message>
+        <source>Switch between US layout and user selected layout</source>
+        <translation>Switch between US layout and user selected layout</translation>
+    </message>
+    <message>
+        <source>&amp;Back</source>
+        <translation>&amp;Back</translation>
+    </message>
+    <message>
+        <source>&amp;Next</source>
+        <translation>&amp;Next</translation>
+    </message>
+    <message>
+        <source>Welcome to your new PC-BSD system! You may change your language above, and click next to begin the setup process.</source>
+        <translation type="vanished">Welcome to your new PC-BSD system! You may change your language above, and click next to begin the setup process.</translation>
+    </message>
+    <message>
+        <source>System Timezone</source>
+        <translation>System Timezone</translation>
+    </message>
+    <message>
+        <source>Root Password</source>
+        <translation>Root Password</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <source>Password (Repeat)</source>
+        <translation>Password (Repeat)</translation>
+    </message>
+    <message>
+        <source>The system or &quot;root&quot; password allows you to make changes to your operating system, and should be kept from non-trusted users. </source>
+        <translation>The system or &quot;root&quot; password allows you to make changes to your operating system, and should be kept from non-trusted users. </translation>
+    </message>
+    <message>
+        <source>Create a User</source>
+        <translation>Create a User</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Username</translation>
+    </message>
+    <message>
+        <source>Connect to a Wireless Network</source>
+        <translation>Connect to a Wireless Network</translation>
+    </message>
+    <message>
+        <source>&amp;Rescan</source>
+        <translation>&amp;Rescan</translation>
+    </message>
+    <message>
+        <source>The following wireless networks were found. Click the one you wish to connect to. If you wish to skip, click &quot;Next&quot; below. </source>
+        <translation>The following wireless networks were found. Click the one you wish to connect to. If you wish to skip, click &quot;Next&quot; below. </translation>
+    </message>
+    <message>
+        <source>Setup is now complete! Press Finish to login to your new desktop.</source>
+        <translation type="vanished">Setup is now complete! Press Finish to login to your new desktop.</translation>
+    </message>
+    <message>
+        <source>&amp;Finish</source>
+        <translation>&amp;Finish</translation>
+    </message>
+    <message>
+        <source>Change the installation language</source>
+        <translation>Change the installation language</translation>
+    </message>
+    <message>
+        <source>Set the system timezone</source>
+        <translation>Set the system timezone</translation>
+    </message>
+    <message>
+        <source>Display help for the current screen</source>
+        <translation>Display help for the current screen</translation>
+    </message>
+    <message>
+        <source>Launch the on-screen keyboard</source>
+        <translation>Launch the on-screen keyboard</translation>
+    </message>
+    <message>
+        <source>Will encrypt users home-directory</source>
+        <translation type="vanished">Will encrypt users home-directory</translation>
+    </message>
+    <message>
+        <source>Encrypt user files</source>
+        <translation type="vanished">Encrypt user files</translation>
+    </message>
+    <message>
+        <source>System Hostname</source>
+        <translation>System Hostname</translation>
+    </message>
+    <message>
+        <source>Enter a valid hostname, or leave default.</source>
+        <translation type="vanished">Enter a valid hostname, or leave default.</translation>
+    </message>
+    <message>
+        <source>Please enter a hostname</source>
+        <translation>Please enter a hostname</translation>
+    </message>
+    <message>
+        <source>Hostname may only contain letters and numbers</source>
+        <translation>Hostname may only contain letters and numbers</translation>
+    </message>
+    <message>
+        <source>Please select the timezone for your location.</source>
+        <translation type="vanished">Please select the timezone for your location.</translation>
+    </message>
+    <message>
+        <source>May be used when dual-booting</source>
+        <translation>May be used when dual-booting</translation>
+    </message>
+    <message>
+        <source>Set BIOS to UTC time</source>
+        <translation>Set BIOS to UTC time</translation>
+    </message>
+    <message>
+        <source>Welcome to your new system! You may change your language above, and click next to begin the setup process.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Domain Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>User Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PersonaCrypt users need the same UID for every system on which they are used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Specify UID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PersonaCrypt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Initialize PersonaCrypt Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Removable Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>User-portion of the disk encryption key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Device Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default Output:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Testing Volume:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Configure Audio Output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Note: Audio settings can also be adjusted later through the audio mixer.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Optional Services</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Setup is now complete! Press Finish to login to your new system.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please enter a domain name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Domain name may only contain letters and numbers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No Devices Found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please connect a removable device and try again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select Removable Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Warning: Any existing data on the selected device will be deleted during the user creation process.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>This screen allows you to select the system language you wish to use.</source>
+        <translation>This screen allows you to select the system language you wish to use.</translation>
+    </message>
+    <message>
+        <source>This screen lets you pick the default timezone for your computer.</source>
+        <translation>This screen lets you pick the default timezone for your computer.</translation>
+    </message>
+    <message>
+        <source>On this screen please enter the root password you wish to use. This password is used for full system access, and should be something secure.</source>
+        <translation>On this screen please enter the root password you wish to use. This password is used for full system access, and should be something secure.</translation>
+    </message>
+    <message>
+        <source>On this screen you will need to create a user to log into your desktop. Additional users may be created in the Control Panel.</source>
+        <translation type="vanished">On this screen you will need to create a user to log into your desktop. Additional users may be created in the Control Panel.</translation>
+    </message>
+    <message>
+        <source>On this screen you may connect to an available wifi network. If you have a hidden network, it may be added later in the Network Manager.</source>
+        <translation>On this screen you may connect to an available wifi network. If you have a hidden network, it may be added later in the Network Manager.</translation>
+    </message>
+    <message>
+        <source>The computer setup is now finished! Click Finish to log into your new Desktop.</source>
+        <translation>The computer setup is now finished! Click Finish to log into your new Desktop.</translation>
+    </message>
+    <message>
+        <source>On this screen you will need to create a user to log into your desktop. Additional users may be created in the SysAdm utility later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This screen allows you to test your audio playback device, and set that device as the default for all audio. This may be changed on-demand through the audio mixer later as well.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Optional configuration changes or services may now be enabled as needed. Simply check or uncheck any options and click next to continue.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable SSH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allows SSH access to this system from remote systems</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Disable IPv6 (Requires Reboot)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Disable the use of IPv6 connections</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Restarting network...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Network is restarting, please wait...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Setting default route...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Running DHCP...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dialogHelp</name>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Close</translation>
+    </message>
+</context>
+<context>
+    <name>dialogInfoBox</name>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Close</translation>
+    </message>
+</context>
+<context>
+    <name>netKey</name>
+    <message>
+        <source>SSID</source>
+        <translation>SSID</translation>
+    </message>
+    <message>
+        <source>Network Key</source>
+        <translation>Network Key</translation>
+    </message>
+    <message>
+        <source>Network Key (Repeat)</source>
+        <translation>Network Key (Repeat)</translation>
+    </message>
+    <message>
+        <source>Show network key as text</source>
+        <translation>Show network key as text</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Apply</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <source>Network Key Error</source>
+        <translation>Network Key Error</translation>
+    </message>
+    <message>
+        <source>Error: The entered network keys do not match!
+</source>
+        <translation>Error: The entered network keys do not match!
+</translation>
+    </message>
+    <message>
+        <source>Security Type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>widgetKeyboard</name>
+    <message>
+        <source>Keyboard Settings</source>
+        <translation>Keyboard Settings</translation>
+    </message>
+    <message>
+        <source>Key Layout</source>
+        <translation>Key Layout</translation>
+    </message>
+    <message>
+        <source>Variant</source>
+        <translation>Variant</translation>
+    </message>
+    <message>
+        <source>Keyboard Model</source>
+        <translation>Keyboard Model</translation>
+    </message>
+    <message>
+        <source>( you may type into the space below to test your selected settings. )</source>
+        <translation>( you may type into the space below to test your selected settings. )</translation>
+    </message>
+    <message>
+        <source>&amp;Apply</source>
+        <translation>&amp;Apply</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Close</translation>
+    </message>
+</context>
+</TS>
