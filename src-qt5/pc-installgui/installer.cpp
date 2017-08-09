fc7a33340cc5d1e8fa91408a17845d49c2a45e2c
Ken Moore <ken@ixsystems.com>
2017-08-09 15:27:47 -0400

    Chase some package name changes upstream.

--- src-qt5/pc-installgui/installer.cpp
+++ src-qt5/pc-installgui/installer.cpp
@@ -1668,7 +1668,7 @@ QStringList Installer::getDeskPkgCfg()
      pkgList << "mail/thunderbird" << "mail/trojita";
 
      // Multimedia player
-     pkgList << "multimedia/vlc" << "multimedia/openh264";
+     pkgList << "multimedia/vlc" << "multimedia/openh264" << "multimedia/pianobar";
 
      // VirtualBox
      //pkgList << "misc/trueos-meta-virtualbox";
@@ -1684,7 +1684,7 @@ QStringList Installer::getDeskPkgCfg()
      pkgList << "graphics/xsane" << "print/qpdfview";
 
      // Printer packages
-     pkgList << "print/cups-pdf" << "print/gutenprint-cups";
+     pkgList << "print/cups-pdf" << "print/cups-filters" << "print/gutenprint";
 
      // Include i18n stuff?
      if ( comboLanguage->currentIndex() != 0 )
