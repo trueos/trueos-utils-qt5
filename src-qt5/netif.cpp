26c25efed5e0a792f287ffe006eee116cde7417e
Ken Moore <ken@ixsystems.com>
2018-06-20 08:38:39 -0400

    Remove the usage of IFM_[TOKEN/FDDI] from trueos-utils-qt5 These flags no longer exist in upstream FreeBSD.

--- src-qt5/netif.cpp
+++ src-qt5/netif.cpp
@@ -145,11 +145,6 @@ QString NetworkInterface::mediaStatusAsString()
 
    switch (IFM_TYPE(ifm.ifm_active))
    {
-      case IFM_FDDI:
-      case IFM_TOKEN:
-         if (ifm.ifm_status & IFM_ACTIVE) status = "inserted";
-         else status = "no ring";
-         break;
       case IFM_IEEE80211:
          if (ifm.ifm_status & IFM_ACTIVE) status = "associated";
          else status = "no carrier";
